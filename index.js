2d5c902c8fdaaf54e439292e3e38864b8758bb6a
My Name <thesame@email.com>
2021-05-04 09:21:27 +0200

    nav is blurred version1

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+var lastScrollTop = 0;
+// Detect the scroll.
+window.addEventListener(
+  "scroll",
+  function() {
+    let st = window.pageYOffset || document.documentElement.scrollTop; 
+    // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop) {
+      // Downscroll code
+      // console.log("\n\ngoing down V\n\n");
+      document.getElementById("navigation").classList.add("blurred");
+    } else {
+      // Upscroll code
+      // console.log("\n\ngoing up ^\n\n");
+      document.getElementById("navigation").classList.remove("blurred");
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  },
+  false
+);
