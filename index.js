1d05439e870452daff274a084bf27657d775f21d
My Name <thesame@email.com>
2021-05-04 13:22:37 +0200

    changing background, needs trnasitions

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+ var picPaths = ['img/bck1.png','img/bck2.png','img/bck3.png'];
+            var curPic = -1;
+            //preload the images for smooth animation
+            var imgO = new Array();
+            for(i=0; i < picPaths.length; i++) {
+                imgO[i] = new Image();
+                imgO[i].src = picPaths[i];
+            }
+
+            function swapImage() {
+                curPic = (++curPic > picPaths.length-1)? 0 : curPic;
+                imgCont.src = imgO[curPic].src;
+                setTimeout(swapImage,2000); 
+
+            }
+
+            window.onload=function() {
+                imgCont = document.getElementById('imgBanner');
+                swapImage();
+            }
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
